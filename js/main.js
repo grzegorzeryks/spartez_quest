1bb8d8887c2968ad0d4649801d678e69a7f82cf0
sarmacki <grzegorzeryks@gmail.com>
2018-10-03 12:42:06 +0200

    basic structure

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var mainNavItem = document.querySelector('.main-nav--active');
+var allNavItems = document.querySelectorAll('header a');
+
+
+// adding new class to an a element and removing it from the rest
+for (var i = 0; i < allNavItems.length; i++) {
+  allNavItems[i].addEventListener('click', function() {
+    if (this.className !== 'main-nav--active') {
+      allNavItems.forEach(function(elem) {
+        elem.classList.remove('main-nav--active');
+      });
+      this.classList.add('main-nav--active');
+    }
+  });
+}
